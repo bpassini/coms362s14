066f1eb8277b9a321621d485d3b87e5403a4f31e
Mike <Mike@Mike-Laptop>
2014-03-05 16:55:03 -0600

    Changed Date to String

--- ResortManagement/src/edu/iastate/cs362/iteration1/HotelControllerInterface.java
+++ ResortManagement/src/edu/iastate/cs362/iteration1/HotelControllerInterface.java
@@ -4,5 +4,5 @@ public interface HotelControllerInterface {
 	boolean createHotel(String hid, String name);
 	boolean addRoom(int beds, int baths, String desc, int rmid);
 	boolean createRoomInvoice(int rmid, String notes);
-	boolean createRoomReservation(String hotel, Date start, Date end, Customer cust, List<Attribute> attr);
+	boolean createRoomReservation(String hotel, String start, Date end, Customer cust, List<Attribute> attr);
 }
